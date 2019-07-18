c5bd198e43a203a1232f1e0829425856efe5d0dd
BIT1120161929 <38394153+BIT1120161929@users.noreply.github.com>
2019-07-18 17:24:25 +0800

    完成基本功能

--- assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
+++ assignment/VideoPlayer/app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -1,19 +1,45 @@
 package com.bytedance.videoplayer;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.view.OrientationEventListener;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.VideoView;
 
 import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
+    private VideoView videoView;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.imageView);
-        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
-        Glide.with(this).load(url).into(imageView);
+
+        videoView = findViewById(R.id.vv_videoview);
+        videoView.setVideoPath(getVideoPath(R.raw.bytedance));
+        MediaController mc = new MediaController(MainActivity.this);
+        videoView.setMediaController(mc);
+        videoView.start();
+    }
+
+    private String getVideoPath(int resId) {
+        return "android.resource://" + this.getPackageName() + "/" + resId;
     }
 }
